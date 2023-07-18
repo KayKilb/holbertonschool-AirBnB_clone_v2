9e09f13d8379b9e680086eae8f8410dcd7fd1063
KayKilb <115676011+KayKilb@users.noreply.github.com>
2023-07-18 14:34:53 -0500

    def hello change

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,13 +3,12 @@
 
 
 from flask import Flask
+
 app = Flask(__name__)
 
-
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """Hello world for flask"""
-    return "Hello HBNB!"
+def hello():
+    return 'Hello HBNB!'
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
