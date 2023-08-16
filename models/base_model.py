f6cd961e46137538595777808b249a5bb9f4ef8c
KayKilb <115676011+KayKilb@users.noreply.github.com>
2023-08-16 10:16:08 -0500

    updated py files to make them executable

--- models/base_model.py
+++ models/base_model.py
@@ -1,47 +1,45 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
-import models
-from uuid import uuid4
-from datetime import datetime
-from sqlalchemy import Column, DateTime, String
-from sqlalchemy.ext.declarative import declarative_base
+import uuid
 from os import getenv
+from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime
+import models
 
-STO_TYP = getenv("HBNB_TYPE_STORAGE")
-if STO_TYP == 'db':
-    Base = declarative_base()
-else:
-    class Base:
-        pass
+Base = declarative_base()
+# Date format
+date = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel:
     """A base class for all hbnb models"""
-    if STO_TYP == 'db':
-        id = Column(String(60), primary_key=True, nullable=False)
-        created_at = Column(DateTime,
-                            default=datetime.utcnow(), nullable=False)
-        updated_at = Column(DateTime,
-                            default=datetime.utcnow(), nullable=False)
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        id = Column(String(60), primary_key=True, unique=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
 
     def __init__(self, *args, **kwargs):
+        """Instatntiates a new model"""
         if not kwargs:
-            kwargs = {}
-        kwargs.setdefault('id', str(uuid4()))
-        kwargs.setdefault('created_at', datetime.utcnow())
-        if not isinstance(kwargs['created_at'], datetime):
-            kwargs['created_at'] = datetime.strptime(
-                kwargs['created_at'], "%Y-%m-%d %H:%M:%S.%f"
-            )
-        kwargs.setdefault('updated_at', datetime.utcnow())
-        if not isinstance(kwargs['updated_at'], datetime):
-            kwargs['updated_at'] = datetime.strptime(
-                kwargs['updated_at'], "%Y-%m-%d %H:%M:%S.%f"
-            )
-        if STO_TYP != 'db':
-            kwargs.pop('__class__', None)
-        for attr, val in kwargs.items():
-            setattr(self, attr, val)
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                if k != "__class__" and k != "_sa_instance_state":
+                    setattr(self, k, v)
+            if "created_at" not in kwargs:
+                self.created_at = datetime.now()
+            if "updated_at" not in kwargs:
+                self.updated_at = datetime.now()
+            if "id" not in kwargs:
+                setattr(self, 'id', str(uuid.uuid4()))
+
+    def delete(self):
+        models.storage.delete(self)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -55,18 +53,12 @@ class BaseModel:
         models.storage.save()
 
     def to_dict(self):
-        """returns BaseModel dictionary"""
-        cls_nam = self.__class__.__name__
-        richard = {
-            k: v if type(v) == str else str(v)
-            for k, v in self.__dict__.items()
-        }
-        richard.update({
-            '__class__': cls_nam
-            })
-        richard.pop("_sa_instance_state", None)
-        return richard
-
-    def delete(self):
-        """deletes basemodel in storage"""
-        models.storage.delete(self)
+        """Convert instance into dict format"""
+        dictionary = {}
+        dictionary = self.__dict__.copy()
+        dictionary['__class__'] = self.__class__.__name__
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+        if '_sa_instance_state' in self.__dict__:
+            del self.__dict__['_sa_instance_state']
+        return dictionary
--- models/user.py
+++ models/user.py
@@ -1,46 +1,30 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-import hashlib
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from sqlalchemy import DateTime
+from sqlalchemy.sql.schema import Column, ForeignKey
+from sqlalchemy.sql.sqltypes import String, Integer
+
+from sqlalchemy.orm import relationship, backref
 from os import getenv
-STO_TYP = getenv("HBNB_TYPE_STORAGE")
 
 
-class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    if STO_TYP == 'db':
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    class User(BaseModel, Base):
+        """This class defines a user by various attributes"""
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship('Place', backref='user',
-                              cascade='all, delete, delete-orphan')
-        reviews = relationship('Review', backref='user',
-                               cascade='all, delete, delete-orphan')
-    else:
+        first_name = Column(String(128))
+        last_name = Column(String(128))
+        places = relationship("Place", backref="user",
+                              cascade="all, delete, delete-orphan")
+        reviews = relationship("Review", backref="user",
+                               cascade="all, delete, delete-orphan")
+else:
+    class User(BaseModel):
+        """This class defines a user by various attributes"""
         email = ''
         password = ''
         first_name = ''
         last_name = ''
-
-    def __init__(self, *args, **kwargs):
-        """
-            instantiates user object
-        """
-        if kwargs:
-            pwd = kwargs.pop('password', None)
-            if pwd:
-                User.__set_password(self, pwd)
-        super().__init__(*args, **kwargs)
-
-    def __set_password(self, pwd):
-        """
-            custom setter: encrypts password to MD5
-        """
-        secure = hashlib.md5()
-        secure.update(pwd.encode("utf-8"))
-        secure_password = secure.hexdigest()
-        setattr(self, "password", secure_password)
